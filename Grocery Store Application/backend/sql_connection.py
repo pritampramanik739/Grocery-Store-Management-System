7fd6870c4395d5b71fdb8cb56c4d8a5881cc5e4e
Pritam Pramanik <pritampramanik739@gmail.com>
2025-08-04 12:23:00 +0530

    Add files via upload

---
+++ Grocery Store Application/backend/sql_connection.py
@@ -0,0 +1,14 @@
+import datetime
+import mysql.connector
+
+__cnx = None
+
+def get_sql_connection():
+  print("Opening mysql connection")
+  global __cnx
+
+  if __cnx is None:
+    __cnx = mysql.connector.connect(user='root', password='Prit@m739SQL', database='grocery_store_application')
+
+  return __cnx
+
